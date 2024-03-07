a8d8c32e075284bd2ec2ae848c9e41c34cd2c588
Daniel Roth <daroth@microsoft.com>
2024-03-07 11:38:06 -0800

    Add some notes

--- WebFrontend/Note.cs
+++ WebFrontend/Note.cs
@@ -2,5 +2,20 @@
 
 public class Note
 {
+    public Note() { }
+    public Note(int id, string title, string content)
+    {
+        Id = id;
+        Title = title;
+        CreatedAt = DateTime.Now;
+        UpdatedAt = CreatedAt;
+        Content = content;
+    }
+
     public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool WasUpdated => UpdatedAt > CreatedAt;
+    public string Content { get; set; }
 }
